152b3430ebd10ab6b2d398e726d2729143115cd4
shubhamtest212 <shubhamjogdandtest@gmail.com>
2026-07-07 16:31:05 +0530

    new changes

--- tests/basics.ts
+++ tests/basics.ts
@@ -4,47 +4,47 @@ let name = 'Shubham'
 console.log(`hello ${name}`)
 
 // Variables
-let a =10;
-a=20;
+let a = 10;
+a = 20;
 let x;
-x=10;
+x = 10;
 
 let num1: number = 10;
 let name1: string = 'Shubham';
 let learningTypescript: boolean = true;
 
-let u: undefined=undefined
-let n: null=null
+let u: undefined = undefined
+let n: null = null
 
 let y;
 console.log(y)
 
 // Arrays
-let list1: number[] = [1,2,3]
-let list2: Array<number> = [1,2,3,4]
+let list1: number[] = [1, 2, 3]
+let list2: Array<number> = [1, 2, 3, 4]
 
 // tuple
-let tuple1:[number,string]; // declare
-tuple1=[1000,'Hello'];// initialize
+let tuple1: [number, string]; // declare
+tuple1 = [1000, 'Hello'];// initialize
 console.log(tuple1[1].substring(0))
 
 // Variables and Data Types
 let myName: string = 'Shubham';
-myName='ShubhamS';
-let age: number=27;
+myName = 'ShubhamS';
+let age: number = 27;
 let isActive: boolean = true;
 
 
 // let
-let employeeName: String ='SHubham';
- console.log(employeeName);
+let employeeName: String = 'SHubham';
+console.log(employeeName);
 
- employeeName = 'Shrikant';
- console.log(employeeName)
+employeeName = 'Shrikant';
+console.log(employeeName)
 
- // const
- const company: string='Firstcry';
- console.log(company);
+// const
+const company: string = 'Firstcry';
+console.log(company);
 //  company='Brainbees';  constant is not getting change
 
 // var
@@ -55,7 +55,7 @@ age1 = 27;
 console.log(age1);
 
 // Boolean
-let isLoggedIn: boolean= true;
+let isLoggedIn: boolean = true;
 console.log(isLoggedIn);
 
 let isQAEngineer: boolean = true;
@@ -76,23 +76,23 @@ console.log(data);
 
 
 let result: any;
-result ="Pass";
-result=100;
-result=false;
+result = "Pass";
+result = 100;
+result = false;
 console.log(result);
 
 // Unknown
-let value : unknown = "Hello";
-if(typeof value==="string"){
+let value: unknown = "Hello";
+if (typeof value === "string") {
     console.log(value.toUpperCase());
 }
 
 // Array
-let fruits: string[] = ["Apple","Banana","Mango"];
+let fruits: string[] = ["Apple", "Banana", "Mango"];
 console.log(fruits);
 
 // Mixed Array
-let details: (String | number)[] = ["Shubham",27,"QA"];
+let details: (String | number)[] = ["Shubham", 27, "QA"];
 console.log(details);
 
 // Null
@@ -100,7 +100,7 @@ let value1: null = null;
 console.log(value1);
 
 // void- returns nothing
-function printMessage(): void{
+function printMessage(): void {
     console.log("TypeScript practice");
     console.log("Shubham Jogdand QA Engineer Have completed Master of Computer Application");
 }
@@ -110,7 +110,7 @@ printMessage();
 
 
 // void by using an arrow function
-const  printMessage1 = (): void=> {
+const printMessage1 = (): void => {
     console.log("Typescript Practices for Automation QA Engineer");
 };
 
@@ -118,7 +118,7 @@ printMessage1();
 
 let values: unknown = "sHuBhAm";
 
-if(typeof values==="string") {
+if (typeof values === "string") {
     console.log(values.toUpperCase());
 
 } else {
@@ -127,22 +127,39 @@ if(typeof values==="string") {
 
 let valuess: unknown = "SHUBHAM";
 
-if(typeof valuess ==="string") {
+if (typeof valuess === "string") {
     console.log(valuess.toLowerCase());
 } else {
     console.log("value is not a string");
 }
 
 // tuple
-let employeeName1: [number, string] = [101,"Shubham"];
+let employeeName1: [number, string] = [101, "Shubham"];
 console.log(employeeName1);
 
 // Object
 let person: {
-    name:string, age:number, designation:string, dob:number, education:string, subdepartment:string}={
-        name:"shubham",age:27, designation:"Software Developnment Engineer in Test", dob:1999, education:"Master_of_computer_application",subdepartment:"Information Technology & Information Engineering"
-    };
-    console.log(person);
+    name: string, age: number, designation: string, dob: number, education: string, subdepartment: string
+} = {
+    name: "shubham", age: 27, designation: "Software Developnment Engineer in Test", dob: 1999, education: "Master_of_computer_application", subdepartment: "Information Technology & Information Engineering"
+};
+console.log(person);
+
+
+// OBJECT
+let personal: {
+    name: string, age: number, designation: string, dob: number, education: string, subdepartment: string
+} = {
+    name: "shubham", age: 27, designation: "SDETEngineer", dob: 1999, education: "MCA", subdepartment: "IT"
+};
+console.log(personal);
+
+
+// Tuple
+let employeeDetails: [string, string, number] = ["shubham", "jogdand", 1999];
+console.log(employeeDetails);
+
+
 
 
 

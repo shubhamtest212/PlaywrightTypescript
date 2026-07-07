644a22c7494cb181b8e1e5148fe4ebcc4a0d07fa
shubhamtest212 <shubhamjogdandtest@gmail.com>
2026-07-07 15:35:07 +0530

    New code commited

--- tests/eximSystemlogin.spec.ts
+++ tests/eximSystemlogin.spec.ts
@@ -7,16 +7,24 @@ test('Exim Login', async ({ page }) => {
 
 });
 
-test('RB System', async ({page}) => {
+test('RB System', async ({ page }) => {
 
     console.log('..I am login in the Rocketbees System..');
     await page.goto('https://stagecourier.rocketbees.in/rb_b2c_cms/');
 
 });
 
-test('Facebook Login', async ({page}) => {
+test('Facebook Login', async ({ page }) => {
 
     console.log('..I am login in the Facebook System..');
     await page.goto('https://www.facebook.com');
+
+});
+
+test('Google', async ({ page }) => {
+
+    console.log('..I am entering some keywords in the google..');
+    await page.goto('https://www.google.com');
+    await page.getByPlaceholder('Search Google or type a URL').fill('Shubham');
 
 });
